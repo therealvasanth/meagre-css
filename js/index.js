841c0ea94f5900b947d9c6346b5f36a1869b9f4c
Vasanth Selvam <therealvasanth@gmail.com>
2017-09-27 00:07:07 +0530

    modifying the right margin of side nav to hide it completely

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 var menuButton = document.getElementById("side-nav");
 
 var displaySideNav = function() {
-    menuButton.style.margin = "0px 0px 0px 0px"; 
+    menuButton.style.margin = "0px -10px 0px 0px"; 
     menuButton.style.WebkitTransition = "all linear 0.3s";
     menuButton.style.transition = "all linear 0.3s"; 
 }
 
 var hideSideNav = function() {
-    menuButton.style.margin = "0px -140px 0px 0px"; 
+    menuButton.style.margin = "0px -150px 0px 0px"; 
     menuButton.style.WebkitTransition = "all linear 0.3s";
     menuButton.style.transition = "all linear 0.3s"; 
 }
